912eaae3ad123f94b2a1bb0bf9e38c0963e6b89c
Wesley Bomar <wbomar@tacc.utexas.edu>
2022-05-09 16:23:05 -0500

    wip-feat(fp-1487): rename to tacc_core_cms_backend
    This matches core… let's see what breaks.
    
    If it works, then try removing duplicate files.

--- tacc_core_cms_backend/asgi.py
+++ tacc_core_cms_backend/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_cms_quickstart_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tacc_core_cms_backend.settings')
 
 application = get_asgi_application()
--- tacc_core_cms_backend/wsgi.py
+++ tacc_core_cms_backend/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_cms_quickstart_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tacc_core_cms_backend.settings')
 
 application = get_wsgi_application()
