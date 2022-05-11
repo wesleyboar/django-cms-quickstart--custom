10ec9eeae1c2e97c9bddb999f9981de6ccd68f64
Wesley Bomar <wbomar@tacc.utexas.edu>
2022-05-11 12:49:10 -0500

    feat(fp-1487): support secrets & settings_custom

--- tacc_core_cms_backend_frontera/settings.py
+++ tacc_core_cms_backend_frontera/settings.py
@@ -1,7 +1,17 @@
 from tacc_core_cms_backend.settings import *
 
 # Application definition
-
 INSTALLED_APPS = [
     'tacc_core_cms_backend_frontera',
 ] + INSTALLED_APPS
+
+# Import local settings
+try:
+    from .secrets import *
+except ModuleNotFoundError:
+    pass
+
+try:
+    from .settings_local import *
+except ModuleNotFoundError:
+    pass
